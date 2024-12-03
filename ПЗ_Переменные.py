ed109e2260a21c05748cd51be0e93e026dbe940c
OlegBlef-spets <bradyagamistific@gmail.com>
2024-12-03 12:58:42 +1000

    Add files via upload

---
+++ ПЗ_Переменные.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework_assignments=12
+Number_of_hours_spent=1,5 #float
+Course_Title="Python"
+Time_for_one_task=(1.5/12)
+print("Курс",Course_Title,"всего задач:",Number_of_completed_homework_assignments,"затрачено часов:",Number_of_hours_spent,"среднее время выполнения",Time_for_one_task,"часа")
