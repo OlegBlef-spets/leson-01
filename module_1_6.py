a9220bdbc84863d180104b4950d7456b1b4b6dca
OlegBlef-spets <bradyagamistific@gmail.com>
2024-12-05 20:19:21 +1000

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -3,12 +3,14 @@ print(my_dict)
 print (my_dict["Sima"])
 my_dict["Sima"]=2025
 print(my_dict)
-my_dict={"Anton" :2041,"Antonina" :2000}
+my_dict.update({"Anton" :2041,"Antonina" :2000})
 print(my_dict)
 my_dict.pop("Anton")
 print(my_dict)
 my_set_={1,2,3,4,5,"String",True,(1,2)}
 print(my_set_)
-my_set_={6,7}
 print(my_set_)
-print(my_set_.diskard(1))
+my_set_.discard(5)
+print(my_set_)
+my_set_.remove(1)
+print(my_set_)
