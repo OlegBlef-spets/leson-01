adbc68df60658a7f46128d62cd75223bb9631f99
OlegBlef-spets <bradyagamistific@gmail.com>
2024-12-05 19:31:05 +1000

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict={"Sima" : 2020,"Julia" : 2001,"Anna" : 1999}#словарь
+print(my_dict)
+print (my_dict["Sima"])
+my_dict["Sima"]=2025
+print(my_dict)
+my_dict={"Anton" :2041,"Antonina" :2000}
+print(my_dict)
+my_dict.pop("Anton")
+print(my_dict)
+my_set_={1,2,3,4,5,"String",True,(1,2)}
+print(my_set_)
+my_set_={6,7}
+print(my_set_)
+print(my_set_.diskard(1))
